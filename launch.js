7cde5753285dbd46ae82f447ce98956ad9b098a8
percentcer <spence.miller@gmail.com>
2013-07-11 00:14:55 +0000

    added files

---
+++ launch.js
@@ -0,0 +1,18 @@
+chrome.browserAction.onClicked.addListener(
+    function( tab ) {
+        var srcURL = tab.url;
+        
+        if ( !srcURL.match(/^https?:\/\/.*youtube.com\/watch/) ) {
+            alert("must be run from a youtube.com watch page");
+            return;
+        }
+
+        var dstURL = 'http://surprise.ly/v/?';
+
+        var qstring = srcURL.split("?")[1];
+
+        var ytid   = qstring.match(/v=([^&]+)/)[1];
+
+        chrome.tabs.create({ 'url' : dstURL + ytid });
+    }
+);
